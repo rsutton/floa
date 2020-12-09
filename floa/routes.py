3eaee32b9a5adada9e901b3706db33d0a185b5f8
Ray Sutton <rsutton@barracuda.com>
2020-12-09 11:13:52 -0500

    refactor update method

--- floa/routes.py
+++ floa/routes.py
@@ -17,8 +17,8 @@ def home():
 def find_by_id(id):
     for i in library:
         if i['id'] == int(id):
-            return i
-    return "id {} not found".format(id)
+            return render_template('list.html', list=[i])
+    return render_template('list.html', list=[{'id': id, 'title': "Item does not exist"}])
 
 @bp.route("/have/<id>")
 def have(id):
@@ -43,14 +43,13 @@ def find_title(query):
     for i in library:
         if q in i['title'].lower():
             results.append(i)
-    return jsonify(results)
+    return render_template('list.html', list=results)
 
 @bp.route("/list")
 def list():
-    return jsonify(library)
+    return render_template('list.html', list=library)
 
-@bp.route("/update")
-def check_for_update():
+def get_latest_loa_catalog():
     ''' webscrape to create a list of LoA titles '''
     import requests
     from bs4 import BeautifulSoup  
@@ -59,37 +58,65 @@ def check_for_update():
 
     soup = BeautifulSoup(page.content, 'html.parser')
     books = soup.find_all('li', class_='content-listing--book')
-    loa_new = []
+    result = []
 
     for book in books:
         id = int(book.find('i', class_='book-listing__number').text)
         title = book.find('b', class_='content-listing__title').text
         book = {"id": id, "title": title}
-        loa_new.append(book)
+        result.append(book)
+    return result
 
-    loa_old = json.load(open('./floa/loa.json', 'r'))
-    loa_diff = [i for i in loa_new + loa_old if i not in loa_new or i not in loa_old]
+def get_list_difference(list1, list2):
+    diff = [i for i in list1 + list2 if i not in list1 or i not in list2]
+    return diff
 
-    message = "No updates, {} records".format(len(loa_new))
+@bp.route("/update")
+def check_for_update():
+    load_catalog()
+
+    loa_latest = get_latest_loa_catalog()
+    loa_diff = get_list_difference(loa_latest, catalog)
+
+    message = "No updates, {} records".format(len(loa_latest))
     if len(loa_diff) > 0:
         message = "Found {} new records {}".format(len(loa_diff), loa_diff)
-        # loa_new is authoritative so overwrite 
-        json.dump(loa_new, open('./floa/loa.json', 'w'))
+        # loa_latest is authoritative so overwrite 
+        overwrite_catalog_with(loa_latest)
         for i in loa_diff:
-            add(i['id'], i['title'])
+            add_to_library(i['id'], i['title'])
 
     return message
 
-def add(id, title):
-    library.append({'id': id, 'title': title, 'have': 0, 'want': 0})
-    save_library()
+
+def write_list_to_file(lst, fname):
+    json.dump(lst, open(fname, 'w+'))
+
+def read_list_from_file(fname):
+    return json.load(open(fname, 'r'))
 
 def save_library():
-    json.dump(library, open('./floa/library.json', 'w'))
+    write_list_to_file(library, './floa/library.json')
     load_library()
 
 def load_library():
     global library 
-    library = json.load(open('./floa/library.json', 'r'))
-    
+    library = read_list_from_file('./floa/library.json')
+
+def add_to_library(id, title):
+    library.append({'id': id, 'title': title, 'have': 0, 'want': 0})
+    save_library()
+
+def save_catalog():
+    write_list_to_file(catalog, './floa/loa_catalog.json')
+    load_catalog()
+
+def load_catalog():
+    global catalog
+    catalog = read_list_from_file('./floa/loa_catalog.json')
+
+def overwrite_catalog_with(lst):
+    write_list_to_file(lst, './floa/loa_catalog.json')
+    load_catalog()
+
 load_library()
