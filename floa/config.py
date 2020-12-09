964b365476ea4f7e3491b791ac3338343d58f003
Ray Sutton <rsutton@barracuda.com>
2020-12-09 11:31:36 -0500

    config and empty list

--- floa/config.py
+++ floa/config.py
@@ -1,2 +1,5 @@
 DEBUG = True
 FLASK_ENV = 'development'
+LOA_URL = 'https://loa.org/books/loa_collection'
+CATALOG_FILENAME = './floa/loa_catalog.json'
+LIBRARY_FILENAME = './floa/library.json'
--- floa/routes.py
+++ floa/routes.py
@@ -18,7 +18,7 @@ def find_by_id(id):
     for i in library:
         if i['id'] == int(id):
             return render_template('list.html', list=[i])
-    return render_template('list.html', list=[{'id': id, 'title': "Item does not exist"}])
+    return render_template('list.html', list=empty_list(id, "Item does not exist"))
 
 @bp.route("/have/<id>")
 def have(id):
@@ -53,7 +53,7 @@ def get_latest_loa_catalog():
     ''' webscrape to create a list of LoA titles '''
     import requests
     from bs4 import BeautifulSoup  
-    url = 'https://loa.org/books/loa_collection'
+    url = app.config['LOA_URL']
     page = requests.get(url)
 
     soup = BeautifulSoup(page.content, 'html.parser')
@@ -67,10 +67,6 @@ def get_latest_loa_catalog():
         result.append(book)
     return result
 
-def get_list_difference(list1, list2):
-    diff = [i for i in list1 + list2 if i not in list1 or i not in list2]
-    return diff
-
 @bp.route("/update")
 def check_for_update():
     load_catalog()
@@ -78,16 +74,20 @@ def check_for_update():
     loa_latest = get_latest_loa_catalog()
     loa_diff = get_list_difference(loa_latest, catalog)
 
-    message = "No updates, {} records".format(len(loa_latest))
     if len(loa_diff) > 0:
-        message = "Found {} new records {}".format(len(loa_diff), loa_diff)
         # loa_latest is authoritative so overwrite 
         overwrite_catalog_with(loa_latest)
         for i in loa_diff:
             add_to_library(i['id'], i['title'])
+        return render_template('list.html', list=loa_diff)
+    return render_template('list.html', list=empty_list(0, "No Updates Found"))
 
-    return message
+def get_list_difference(list1, list2):
+    diff = [i for i in list1 + list2 if i not in list1 or i not in list2]
+    return diff
 
+def empty_list(id=0, title='NA'):
+    return [{'id': id, 'title': title, 'have': 0, 'want': 0}]
 
 def write_list_to_file(lst, fname):
     json.dump(lst, open(fname, 'w+'))
@@ -96,27 +96,27 @@ def read_list_from_file(fname):
     return json.load(open(fname, 'r'))
 
 def save_library():
-    write_list_to_file(library, './floa/library.json')
+    write_list_to_file(library, app.config['LIBRARY_FILENAME'])
     load_library()
 
 def load_library():
     global library 
-    library = read_list_from_file('./floa/library.json')
+    library = read_list_from_file(app.config['LIBRARY_FILENAME'])
 
 def add_to_library(id, title):
     library.append({'id': id, 'title': title, 'have': 0, 'want': 0})
     save_library()
 
 def save_catalog():
-    write_list_to_file(catalog, './floa/loa_catalog.json')
+    write_list_to_file(catalog, app.config['CATALOG_FILENAME'])
     load_catalog()
 
 def load_catalog():
     global catalog
-    catalog = read_list_from_file('./floa/loa_catalog.json')
+    catalog = read_list_from_file(app.config['CATALOG_FILENAME'])
 
 def overwrite_catalog_with(lst):
-    write_list_to_file(lst, './floa/loa_catalog.json')
+    write_list_to_file(lst, app.config['CATALOG_FILENAME'])
     load_catalog()
 
 load_library()
