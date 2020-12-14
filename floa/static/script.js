72ed2b654d4324251c7dd28d00f4a3bcf6834a1e
Ray Sutton <rsutton@barracuda.com>
2020-12-14 13:41:40 -0500

    cleanup

--- floa/static/script.js
+++ floa/static/script.js
@@ -2,17 +2,19 @@
 $(document).ready(function(){
     var $SCRIPT_ROOT = location.origin;
 
+    // initialize home view
     var activePage = $('.nav-link active').text()
     if ( activePage = "Bookshelf" ){
         $('.content-listing').each(function(){
-            if( $(this).attr('data-status') != '1' ){
-                $(this).hide();
-            } else {
+            if( $(this).attr('data-status') = '1' ){
                 $(this).show();
+            } else {
+                $(this).hide();
             }
         });
     } 
-    // initialize checkboxes
+    // initialize checkbox status
+    // default class is fa-circle-o
     $( '.content-listing' ).each(function(){
         if ($(this).attr('data-status') == '1'){
             $(this).find('i.fa').removeClass('fa-circle-o').addClass('fa-check');
@@ -20,6 +22,8 @@ $(document).ready(function(){
             $(this).find('i.fa').removeClass('fa-circle-o').addClass('fa-heart');
         }
     });
+
+    // tri-state checkbox click handler
     $( '.content-listing__checkbox' ).click(function(e){
         if ( $(this).parent().parent().attr('data-status') == '0' ){
             $(this).children().removeClass('fa-circle-o').addClass('fa-check');
@@ -42,43 +46,26 @@ $(document).ready(function(){
             data: JSON.stringify({"id": id, "status": status})
         });
     });
-    $( '.content-listing__button' ).click(function(){
-        var id = $(this).parent().attr('data-id');
-        var url = $SCRIPT_ROOT + "/_update";
-        $.ajax({
-            url: url,
-            dataType: "json",
-            contentType: "application/json",
-            method: "POST",
-            data: JSON.stringify({"id": id, "status": 0}),
-            success: function(){location.reload()}
-        });
-    });
-    /*
-    $('button[type=submit]').click(function(){
-        var query = $('input[name=search]').val();
-    });
-    */
 
-    // nav processing
+    // navigation handler
     $('a.nav-link').click(function(){ 
         var clicked = $(this).text();
         $('a.nav-link').each(function(){ $(this).removeClass("active"); });
         $(this).addClass("active");
         if ( clicked == "Bookshelf" ){
             $('.content-listing').each(function(){
-                if( $(this).attr('data-status') != '1' ){
-                    $(this).hide();
-                } else {
+                if( $(this).attr('data-status') = '1' ){
                     $(this).show();
+                } else {
+                    $(this).hide();
                 }
             });
         } else if ( clicked == "Wish List" ){
             $('.content-listing').each(function(){
-                if( $(this).attr('data-status') != '2' ){
-                    $(this).hide();
-                } else {
+                if( $(this).attr('data-status') = '2' ){
                     $(this).show();
+                } else {
+                    $(this).hide();
                 }
             });  
         } else {
@@ -87,8 +74,10 @@ $(document).ready(function(){
             });  
         }
     });
-    // bookshelf view - filter status == 1, hide content-listing__controls
-    
-    // wishlist view - filter status == 2
-    // catalog view - no filter
+    /* search
+    $('button[type=submit]').click(function(){
+        var query = $('input[name=search]').val();
+    });
+    */
+
 });
