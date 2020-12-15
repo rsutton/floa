3eae4d96b5b48672e35f604b368bf9f09e065c3d
Ray Sutton <rsutton@barracuda.com>
2020-12-15 09:20:45 -0500

    refactor checkbox processing search prototype

--- floa/static/script.js
+++ floa/static/script.js
@@ -1,10 +1,56 @@
 "use strict";
 $(document).ready(function(){
-    var $SCRIPT_ROOT = location.origin;
+    let $SCRIPT_ROOT = location.origin;
+    let status_icons = ['fa-circle-o', 'fa-check', 'fa-heart']
 
     // initialize home view
-    var activePage = $('.nav-link active').text()
-    if ( activePage = "Bookshelf" ){
+    let activePage = $('.nav-link active').text()
+    showBookshelf();
+
+    // handle checkbox status
+    function iconClassMatcher(index, className){
+        let matchedClasses = className.match(/(^|\s)fa-\S+/g)
+        return (matchedClasses || []).join('')
+    }
+    // initialize status
+    $( '.content-listing' ).each(function(){
+        let status = $(this).attr('data-status')
+        $(this).children('.content-listing__control').removeClass(iconClassMatcher).addClass(status_icons[status]);
+    });
+
+    // tri-state checkbox click handler
+    $( '.content-listing__control' ).click(function(){
+        let current_status = $(this).parent().attr('data-status');
+        let new_status = (current_status + 1) % status_icons.length;
+        $(this).parent().attr('data-status', new_status);
+        $(this).removeClass(iconClassMatcher).addClass(status_icons[new_status]);
+
+        // save status to library
+        let id = $(this).parent().attr('data-id');
+        let url = $SCRIPT_ROOT + "/_update";
+        $.ajax({
+            url: url,
+            dataType: "json",
+            contentType: "application/json",
+            method: "POST",
+            data: JSON.stringify({"id": id, "status": new_status})
+        });
+    });
+
+    // navigation handler
+    $('a.nav-link').click(function(){ 
+        let clicked = $(this).text();
+        $('a.nav-link').each(function(){ $(this).removeClass("active"); });
+        $(this).addClass("active");
+        if ( clicked == "Bookshelf" ){
+            showBookshelf();
+        } else if ( clicked == "Wish List" ){
+            showWishList();
+        } else {
+            showCatalog();
+        }
+    });
+    function showBookshelf(){
         $('.content-listing').each(function(){
             if( $(this).attr('data-status') == '1' ){
                 $(this).show();
@@ -12,72 +58,30 @@ $(document).ready(function(){
                 $(this).hide();
             }
         });
-    } 
-    // initialize checkbox status
-    // default class is fa-circle-o
-    $( '.content-listing' ).each(function(){
-        if ($(this).attr('data-status') == '1'){
-            $(this).find('i.fa').removeClass('fa-circle-o').addClass('fa-check');
-        } else if ($(this).attr('data-status') == '2'){
-            $(this).find('i.fa').removeClass('fa-circle-o').addClass('fa-heart');
-        }
-    });
-
-    // tri-state checkbox click handler
-    $( '.content-listing__checkbox' ).click(function(e){
-        if ( $(this).parent().parent().attr('data-status') == '0' ){
-            $(this).children().removeClass('fa-circle-o').addClass('fa-check');
-            $(this).parent().parent().attr('data-status', '1');
-        } else if ( $(this).parent().parent().attr('data-status') == '1' ){
-            $(this).children().removeClass('fa-check').addClass('fa-heart');
-            $(this).parent().parent().attr('data-status', '2');
-        } else if ( $(this).parent().parent().attr('data-status') == '2' ){
-            $(this).children().removeClass('fa-heart').addClass('fa-circle-o');
-            $(this).parent().parent().attr('data-status', '0');
-        }
-        var id = $(this).parent().parent().attr('data-id');
-        var status = $(this).parent().parent().attr('data-status');
-        var url = $SCRIPT_ROOT + "/_update";
-        $.ajax({
-            url: url,
-            dataType: "json",
-            contentType: "application/json",
-            method: "POST",
-            data: JSON.stringify({"id": id, "status": status})
+    }
+    function showWishList(){
+        $('.content-listing').each(function(){
+            if( $(this).attr('data-status') == '2' ){
+                $(this).show();
+            } else {
+                $(this).hide();
+            }
+        });  
+    }
+    function showCatalog(){
+        $('.content-listing').each(function(){
+            $(this).show();
+        });
+    }
+    // search
+    $('button[type=submit]').click(function(){
+        let query = $('input[name=search]').val().toUpperCase();
+        showCatalog();
+        $('.content-listing').each(function(){
+            if ( $(this).find('.content-listing__title').text().toUpperCase().indexOf(query) == -1 ){
+                $(this).hide();
+            }
         });
     });
-
-    // navigation handler
-    $('a.nav-link').click(function(){ 
-        var clicked = $(this).text();
-        $('a.nav-link').each(function(){ $(this).removeClass("active"); });
-        $(this).addClass("active");
-        if ( clicked == "Bookshelf" ){
-            $('.content-listing').each(function(){
-                if( $(this).attr('data-status') == '1' ){
-                    $(this).show();
-                } else {
-                    $(this).hide();
-                }
-            });
-        } else if ( clicked == "Wish List" ){
-            $('.content-listing').each(function(){
-                if( $(this).attr('data-status') == '2' ){
-                    $(this).show();
-                } else {
-                    $(this).hide();
-                }
-            });  
-        } else {
-            $('.content-listing').each(function(){
-                $(this).show();
-            });  
-        }
-    });
-    /* search
-    $('button[type=submit]').click(function(){
-        var query = $('input[name=search]').val();
-    });
-    */
 
 });
