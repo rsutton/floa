4cde5272bdb541acb019d6e48a944ecacbe4a0b5
Ray Sutton <rsutton@barracuda.com>
2020-12-17 14:43:09 -0500

    update setup

--- setup.py
+++ setup.py
@@ -1,12 +1,27 @@
 from setuptools import find_packages, setup
 
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
 setup(
     name='floa',
     version='1.0.0',
+    author="Ray Sutton",
+    author_email="ray.sutton@gmail.com",
+    description="My Library of America bookshelf manager",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/rsutton/floa",
     packages=find_packages(),
     include_package_data=True,
     zip_safe=False,
     install_requires=[
         'flask', 'bs4', 'requests'
     ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.7',
 )
